b1ded907d8b74623a401ea6765471ac074be6166
Felix Lai <felix.lai@hotmail.com>
2023-02-22 15:08:55 -0500

    passed props into task

--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-export const Task = () => {
+export const Task = (props) => {
   return <div>Task</div>;
 };
