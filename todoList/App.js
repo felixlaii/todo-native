501eddc069e892cd7bf6fb07b124a812e007f2e3
Felix Lai <felix.lai@hotmail.com>
2023-02-22 11:15:13 -0500

    Update App.js

--- todoList/App.js
+++ todoList/App.js
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.tasksWrapper}></View>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Tasks</Text>
+      </View>
     </View>
   );
 }
