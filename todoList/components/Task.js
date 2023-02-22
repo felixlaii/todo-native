924bc0f13c687fb86bac68d11fde705665d77b50
Felix Lai <felix.lai@hotmail.com>
2023-02-22 15:09:38 -0500

    Update Task.js

--- todoList/components/Task.js
+++ todoList/components/Task.js
@@ -2,5 +2,11 @@ import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
 export const Task = (props) => {
-  return <div>Task</div>;
+  return (
+    <View style={styles.item}>
+      <View>
+        <View></View>
+      </View>
+    </View>
+  );
 };
